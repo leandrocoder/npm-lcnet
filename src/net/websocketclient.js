3fc545d3e237e2d7c889eb3c001dd6dca0bdd653
Leandro Carlos <leandro.carlos@mediamonks.com>
2018-12-12 15:28:48 -0200

    basic implementation of websocket client for web

--- src/net/websocketclient.js
+++ src/net/websocketclient.js
@@ -1,11 +1,63 @@
-var WebSocketClientClass = require('websocket').client;
+module.exports = class WebSocketClient {
+    
+    constructor(host, port) {
+        this.events = [];
+        this.isBrowser = (typeof window !== 'undefined' && typeof window.WebSocket !== 'undefined');
+        let address = `ws://${host}:${port}`;
+        this.ws = this.isBrowser == true ? new window.WebSocket(address) : new (require('ws'))(address);
 
+        if (this.isBrowser == true)
+        {
+            this.ws.onmessage = (event) => {
+                let data = event.data;
+                for (let i = 0; i < this.events.length; i++)
+                {
+                    let e = this.events[i];
+                    if (e.event == 'message' && e.callback) e.callback(data);
+                }
+            }
+
+            this.ws.onclose = (event) => {
+                let data = event.data;
+                for (let i = 0; i < this.events.length; i++)
+                {
+                    let e = this.events[i];
+                    if (e.event == 'close' && e.callback) e.callback();
+                }
+            }
+
+            this.ws.onopen = (event) => {
+                for (let i = 0; i < this.events.length; i++)
+                {
+                    let e = this.events[i];
+                    if (e.event == 'open' && e.callback) e.callback();
+                }
+            }
+        }
+    }
+    
+    on (event, callback) {
+
+        this.events.push({event:event, callback:callback});
+        if (this.isBrowser == false)
+        {
+            this.ws.on(event, (...args) => callback(...args));
+        }
+    }
+    
+    send(data)
+    {
+        this.ws.send(data);
+    }
+}
+
+
+/*
+var WebSocketClientClass = require('websocket').client;
 module.exports = class WebSocketClient {
 
     constructor() {
         this.events = [];
-        this.connection = null;
-        this.connected = false;
         this.client = new WebSocketClientClass();
     }
 
@@ -59,3 +111,4 @@ module.exports = class WebSocketClient {
         if (this.connection) this.connection.send(JSON.stringify(obj));
     }
 }
+*/
